cb5d7a86f61de7db4239cccce017527a20ea3c4f
Sébastien Fauvel <sfauvel@gmail.com>
2020-02-25 00:38:26 +0100

    Update documentation with git information

--- check.sh
+++ check.sh
@@ -11,11 +11,11 @@ ALL_GIT_FILES=$(git ls-files $ROOT_DOC)
 NB_FAILURES=$(wc --lines <<<$ALL_FAILING_TESTS)
 
 write_failure() {
-  echo -e "- ${RED}$1${NO_COLOR}"
+  echo -e "${RED}$1${NO_COLOR}"
 }
 
 write_success() {
-  echo -e "- ${GREEN}$1${NO_COLOR}"
+  echo -e "${GREEN}$1${NO_COLOR}"
 }
 
 ALL_FAILING_TESTS_RAW=$(sed -r "s|.*\/(.*).adoc|\1|g" <<<"${DIFF}")
@@ -23,12 +23,13 @@ for filename in $ALL_GIT_FILES
 do
   SIMPLE_FILE_NAME=$(sed -r "s|.*\/(.*).adoc|\1|g" <<<"$filename")
   if [[ $ALL_FAILING_TESTS == *"$filename"* ]]; then
-    write_failure "$SIMPLE_FILE_NAME"
+    write_failure "- $SIMPLE_FILE_NAME"
   else
-    write_success "$SIMPLE_FILE_NAME"
+    write_success "- $SIMPLE_FILE_NAME"
   fi
 done
 
+echo ---------------------
 if [ -z "$ALL_FAILING_TESTS" ]
 then
   write_success "SUCCESS"
