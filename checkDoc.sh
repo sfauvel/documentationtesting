c014a8b3b5602a901cd818dfcb4ca2d81aeca6a7
Sébastien Fauvel <sfauvel@gmail.com>
2020-03-02 09:12:29 +0100

    Simplify and correct script to find diff with git HEAD

--- checkDoc.sh
+++ checkDoc.sh
@@ -5,26 +5,21 @@ ROOT_DOC=$1
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NO_COLOR='\033[0m' # No Color
+NEW_LINE=$'\n'
 
-ALL_FAILING_TESTS=$(git diff --name-only $ROOT_DOC)
-ALL_GIT_FILES=$(git ls-files -dmoc $ROOT_DOC)
+ALL_FAILING_TESTS=$(git status -s $ROOT_DOC)
 
-GIT_DIFF_BETWEEN_HEAD_AND_INDEX=$(git diff-index --no-commit-id --name-only HEAD $ROOT_DOC)
-GIT_NOT_INDEXES_FILES=$(git ls-files -o $ROOT_DOC)
+ALL_GIT_FILES_NOT_DELETED=$(git ls-files -oc $ROOT_DOC)
+DELETED_FILE=$(git diff-index  --diff-filter=D --name-only HEAD $ROOT_DOC)
+ALL_GIT_FILES="$ALL_GIT_FILES_NOT_DELETED$NEW_LINE$DELETED_FILE"
 
-if [ ! -z "$GIT_DIFF_BETWEEN_HEAD_AND_INDEX" ]
+if [ ! -z "$ALL_FAILING_TESTS" ]
 then
-  NB_GIT_DIFF_BETWEEN_HEAD_AND_INDEX=$(wc --lines <<<"$GIT_DIFF_BETWEEN_HEAD_AND_INDEX")
+  NB_FAILURES=$(wc --lines <<<"$ALL_FAILING_TESTS")
 else
-  NB_GIT_DIFF_BETWEEN_HEAD_AND_INDEX=0
+  NB_FAILURES=0
 fi
-if [ ! -z "$GIT_NOT_INDEXES_FILES" ]
-then
-  NB_GIT_NOT_INDEXES_FILES=$(wc --lines <<<"$GIT_NOT_INDEXES_FILES")
-else
-  NB_GIT_NOT_INDEXES_FILES=0
-fi
-NB_FAILURES=$((NB_GIT_DIFF_BETWEEN_HEAD_AND_INDEX + NB_GIT_NOT_INDEXES_FILES))
+
 
 write_failure() {
   echo -e "${RED}$1${NO_COLOR}"
@@ -34,12 +29,10 @@ write_success() {
   echo -e "${GREEN}$1${NO_COLOR}"
 }
 
-for filename in $ALL_GIT_FILES
+for FILENAME in $ALL_GIT_FILES
 do
-  SIMPLE_FILE_NAME=$(sed -r "s|.*\/(.*).adoc|\1|g" <<<"$filename")
-  if [[ $GIT_DIFF_BETWEEN_HEAD_AND_INDEX == *"$filename"* ]]; then
-    write_failure "- $SIMPLE_FILE_NAME"
-  elif [[ $GIT_NOT_INDEXES_FILES == *"$filename"* ]]; then
+  SIMPLE_FILE_NAME=$(sed -r "s|.*\/(.*).adoc|\1|g" <<<"$FILENAME")
+  if [[ $ALL_FAILING_TESTS == *"$FILENAME"* ]]; then
     write_failure "- $SIMPLE_FILE_NAME"
   else
     write_success "- $SIMPLE_FILE_NAME"
