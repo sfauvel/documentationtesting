9e6c270d29bb7b83f49c1c3530e94cd444fe8975
Sébastien Fauvel <sfauvel@gmail.com>
2020-03-04 23:56:56 +0100

    Add option on script to display help and select validation mode.

--- generateDoc.sh
+++ generateDoc.sh
@@ -6,6 +6,29 @@ set -euo pipefail
 # with git: file .approved is compared with git commited version. It detects tests removed.
 VALIDATION_MODE="git"
 
+# Usage info
+function show_help() {
+  echo "Usage: ${0##*/} [-h] [-m VALIDATION_MODE] [FILE]..."
+  echo "Build all the project: compile, generate documentation, verify there is no regression"
+  echo "and convert asciidoctor generated to Html."
+  echo ""
+  echo "    -h                  display this help and exit."
+  echo "    -m VALIDATION_MODE: could be 'git' or 'approvals'."
+}
+
+while getopts ":hm" opt; do
+   case ${opt} in
+     h ) show_help
+       exit 0
+       ;;
+     m ) VALIDATION_MODE=$opt
+       ;;
+     \? ) show_help
+       exit 0
+       ;;
+   esac
+done
+
 function remove_docs_directories() {
   for DEMO_NAME in  $(ls | grep "demo_*")
   do
