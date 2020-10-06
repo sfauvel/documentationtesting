62807a569a30b415e83f885ceff38decee5dd7b6
sfauvel <sfauvel@gmail.com>
2020-10-06 16:43:07 +0200

    Improve Trivia documentation

--- samples/demo_trivia/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
+++ samples/demo_trivia/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -19,6 +20,8 @@ import java.util.stream.IntStream;
 @DisplayName("Explanations and examples")
 public class GameTest extends ApprovalsBase {
 
+
+    private boolean notAWinner = true;
     class FakeGame extends Game {
 
         boolean ask = false;
@@ -37,6 +40,15 @@ public class GameTest extends ApprovalsBase {
         return aGame;
     }
 
+    public void displayInLine(Runnable... displayMethods) {
+        write(String.format("[.tableInline]\n[%%autowidth, cols=%d, frame=none, grid=none]\n|====\n",displayMethods.length));
+        for (Runnable displayMethod : displayMethods) {
+            write("\na|");
+            displayMethod.run();
+        }
+        write("\n|====\n");
+    }
+
     /**
      * In a game turn, the player rolls a dice and advances the number of spaces indicated.
      * @throws Exception
@@ -45,23 +57,50 @@ public class GameTest extends ApprovalsBase {
     @DisplayName("Movements of a player")
     public void player_advances() throws Exception {
 
-        final FakeGame aGame = startGame("Chet");
+        {
+            write("== Normal move\n\n");
 
-        write("== Normal move\n\n");
-        displayPosition(aGame, "start", aGame.currentPlayer);
+            final FakeGame aGame = startGame("Chet");
+            final int currentPlayerNumber = aGame.currentPlayer;
+            final int roll = 4;
 
-        displayRollDice(aGame, 4);
-        write("\n\n");
+            displayInLine(
+                    () -> {
+                        write("[.tableHeader]#Start of the turn#\n");
+                        write("\n\n");
+                        displayPosition(aGame, "start", currentPlayerNumber);
+                    },
+                    () -> {
+                        displayRollDice(aGame, roll);
+                        write("\n\n");
+                        displayPosition(aGame, "after move", currentPlayerNumber);
+                    }
+            );
+        }
+        {
+            write("== Move beyond the end\n\n");
 
-        displayPosition(aGame, "end", aGame.currentPlayer);
+            write("\n\nIf he reaches the end of the board, he continues by starting from the beginning\n\n");
 
-        write("== Move beyond the end\n\n");
+            final FakeGame aGame = startGame("Chet");
+            final int currentPlayerNumber = aGame.currentPlayer;
+            final int roll = 3;
+            aGame.roll(4);
+            aGame.roll(6);
 
-        write("\n\nIf he reaches the end of the board, he continues by starting from the beginning\n\n");
-        aGame.roll(6);
-        displayPosition(aGame, "start", aGame.currentPlayer);
-        displayRollDice(aGame, 5);
-        displayPosition(aGame, "end", aGame.currentPlayer);
+            displayInLine(
+                    () -> {
+                        write("[.tableHeader]#Start of the turn#\n");
+                        write("\n\n");
+                        displayPosition(aGame, "start", currentPlayerNumber);
+                    },
+                    () -> {
+                        displayRollDice(aGame, roll);
+                        write("\n\n");
+                        displayPosition(aGame, "after move", currentPlayerNumber);
+                    }
+            );
+        }
 
         addStyleSheet();
     }
@@ -76,11 +115,11 @@ public class GameTest extends ApprovalsBase {
 
         final FakeGame aGame = startGame("Chet");
 
-        final int playerHighlighted = aGame.currentPlayer;
-        String currentPlayer = aGame.players.get(playerHighlighted).toString();
+        final int currentPlayerNumber = aGame.currentPlayer;
+        String currentPlayer = aGame.players.get(currentPlayerNumber).toString();
 
-        displayPosition(aGame, "start", playerHighlighted);
         displayOneTurn(aGame, currentPlayer, () -> 3, () -> false);
+
         addStyleSheet();
     }
 
@@ -102,8 +141,7 @@ public class GameTest extends ApprovalsBase {
         final int playerHighlighted = aGame.currentPlayer;
         String currentPlayer = aGame.players.get(playerHighlighted).toString();
 
-        displayPosition(aGame, "start", playerHighlighted);
-        final boolean notAWinner = displayOneTurn(aGame, currentPlayer, () -> 3, () -> false);
+        displayOneTurn(aGame, currentPlayer, () -> 3, () -> false);
         if (!notAWinner) {
             write(currentPlayer + " wins the game !!! +\n");
         }
@@ -119,12 +157,11 @@ public class GameTest extends ApprovalsBase {
     @Test
     public void jail() throws Exception {
         {
-            final FakeGame aGame = startGame("Chet");
-
             write("== Got to jail\n\n");
+
+            final FakeGame aGame = startGame("Chet");
             final int playerHighlighted = aGame.currentPlayer;
             String currentPlayer = aGame.players.get(playerHighlighted).toString();
-            displayPosition(aGame, "start", playerHighlighted);
 
             final Supplier<Integer> rollSupplier = () -> 3;
             final Supplier<Boolean> wrongAnswer = () -> true;
@@ -132,24 +169,22 @@ public class GameTest extends ApprovalsBase {
             displayOneTurn(aGame, currentPlayer, rollSupplier, wrongAnswer);
 
             write("== Need an odd number to move\n\n");
-            displayPosition(aGame, "start", playerHighlighted);
 
             displayOneTurn(aGame, currentPlayer, () -> 2, () -> true);
 
             displayOneTurn(aGame, currentPlayer, () -> 3, () -> true);
         }
         {
+            write("== Get out of jail\n\n");
+            write("When player correctly answer to a question, he goes out of jail.\n\n");
+
             final FakeGame aGame = startGame("Chet");
+            aGame.wrongAnswer();
 
             final int playerHighlighted = aGame.currentPlayer;
             String currentPlayer = aGame.players.get(playerHighlighted).toString();
 
-            write("== Get out of jail\n\n");
-            write("When player correctly answer to a question, he goes out of jail.\n\n");
-
-            displayPosition(aGame, "Start", playerHighlighted);
-            displayOneTurn(aGame, currentPlayer, () -> 3, () -> true);
-            displayOneTurn(aGame, currentPlayer, () -> 5, () -> false);
+            displayOneTurn(aGame, currentPlayer, () -> 3, () -> false);
         }
         addStyleSheet();
     }
@@ -162,18 +197,15 @@ public class GameTest extends ApprovalsBase {
     public void player_stay_in_penality_box() throws Exception {
 
         final FakeGame aGame = startGame("Chet");
-
         aGame.roll(3);
         aGame.wrongAnswer();
 
         String currentPlayer = aGame.players.get(aGame.currentPlayer).toString();
 
-        displayPosition(aGame, "start", aGame.currentPlayer);
-
         final Supplier<Integer> rollSupplier = () -> 2;
         final Supplier<Boolean> wrongAnswer = () -> false;
 
-        boolean notAWinner = displayOneTurn(aGame, currentPlayer, rollSupplier, wrongAnswer);
+        displayOneTurn(aGame, currentPlayer, rollSupplier, wrongAnswer);
 
         addStyleSheet();
     }
@@ -191,19 +223,18 @@ public class GameTest extends ApprovalsBase {
 
         Random rand = new Random(12345);
 
-        boolean notAWinner = true;
+        notAWinner = true;
         int tour = 1;
         String currentPlayer;
         do {
 
             currentPlayer = aGame.players.get(aGame.currentPlayer).toString();
             write("*Turn " + tour++ + "*\n\n");
-            displayPosition(aGame, "start", aGame.currentPlayer);
 
             final Supplier<Integer> rollSupplier = () -> rand.nextInt(5) + 1;
             final Supplier<Boolean> wrongAnswer = () -> rand.nextInt(9) == 7;
 
-            notAWinner = displayOneTurn(aGame, currentPlayer, rollSupplier, wrongAnswer);
+            displayOneTurn(aGame, currentPlayer, rollSupplier, wrongAnswer);
             write("\n\n___\n\n");
             if (!notAWinner) {
                 write(currentPlayer + " wins the game !!! +\n");
@@ -219,16 +250,20 @@ public class GameTest extends ApprovalsBase {
                 .collect(Collectors.joining("\n")));
     }
 
-
-    private boolean displayOneTurn(FakeGame aGame, String currentPlayer, Supplier<Integer> rollSupplier, Supplier<Boolean> wrongAnswer) throws IOException {
-        boolean notAWinner = true;
+    private void displayOneTurn(FakeGame aGame, String currentPlayer, Supplier<Integer> rollSupplier, Supplier<Boolean> wrongAnswer) throws IOException {
         final int roll = rollSupplier.get();
 
         final int currentPlayerNumber = aGame.currentPlayer;
 
         aGame.ask = false;
-        displayRollDice(aGame, roll);
+        displayInLine(
+                () -> {write("[.tableHeader]#Start of the turn#\n"); write("\n\n");  displayPosition(aGame, "start", currentPlayerNumber);},
+                () -> {displayRollDice(aGame, roll); write("\n\n");  displayPosition(aGame, "after move", currentPlayerNumber);},
+                () -> {displayQuestionAsked(aGame, currentPlayer, wrongAnswer); write("\n\n"); displayPosition(aGame, "end", currentPlayerNumber);}
+        );
+    }
 
+    private void displayQuestionAsked(FakeGame aGame, String currentPlayer, Supplier<Boolean> wrongAnswer) {
         if (aGame.ask) {
             if (wrongAnswer.get()) {
                 write(String.format("[wrongAnswer]#&#x2718;#\n%s incorrectly answered to %s question +\n", currentPlayer, aGame.currentCategory()));
@@ -240,10 +275,6 @@ public class GameTest extends ApprovalsBase {
         } else {
             write(String.format("No question for %s +\n", currentPlayer));
         }
-
-        displayPosition(aGame, "end", currentPlayerNumber);
-        //displayPlayers(aGame);
-        return notAWinner;
     }
 
     private void displayRollDice(FakeGame aGame, int roll) {
@@ -271,7 +302,62 @@ public class GameTest extends ApprovalsBase {
         write("Categories :\n\n"+line+"\n\n");
     }
 
-    private void displayPosition(Game aGame, String startOrEnd, int playerHighLighted) throws IOException {
+    private void displayPosition(Game aGame, String startOrEnd, int playerHighLighted) {
+
+        final int columns = 12;
+
+        write(String.format("[.boardTitle]\nBoard at the %s of the turn\n\n++++\n\n<table class=\"triviaBoard\">\n", startOrEnd));
+
+        Function<Integer, String> tdPosition = position -> String.format("<td class=\"%s\">%s</td>",
+                aGame.category(position).toLowerCase(),
+                formatPlayers(getListPlayerAt(aGame, position, playerHighLighted), ""));
+
+        int position = 0;
+        write("<tr>\n");
+        write(tdPosition.apply(0));
+        write(tdPosition.apply(1));
+        write(tdPosition.apply(2));
+        write(tdPosition.apply(3));
+        write("</tr>\n");
+
+        write("<tr>\n");
+        write(tdPosition.apply(11));
+        write("<td>&nbsp;</td>");
+        write("<td>&nbsp;</td>");
+        write(tdPosition.apply(4));
+        write("</tr>\n");
+
+        write("<tr>\n");
+        write(tdPosition.apply(10));
+        write("<td>&nbsp;</td>");
+        write("<td>&nbsp;</td>");
+        write(tdPosition.apply(5));
+        write("</tr>\n");
+
+        write("<tr>\n");
+        write(tdPosition.apply(9));
+        write(tdPosition.apply(8));
+        write(tdPosition.apply(7));
+        write(tdPosition.apply(6));
+        write("</tr>\n");
+
+//        write("<tr>\n"+IntStream.range(0, columns)
+//                .mapToObj(String.format("<td class=\"%s\">%s</td>",
+//                        aGame.category(position).toLowerCase(),
+//                        getListPlayerAt(aGame, position, playerHighLighted).stream().collect(Collectors.joining("")))
+//                + "\n</tr>\n"));
+        write("</table>\n\n++++\n\n");
+    }
+
+    private String formatPlayers(List<String> listPlayerAt, String separator) {
+        if (listPlayerAt.isEmpty()) {
+            return "&nbsp;";
+        }
+
+        return listPlayerAt.stream().collect(Collectors.joining(separator));
+    }
+
+    private void displayPositionXX(Game aGame, String startOrEnd, int playerHighLighted) throws IOException {
 //        write("[%autowidth]\n|====\n");
         final int columns = 12;
 //        write("Board :\n[cols=\"12*a\"]\n|====\n");
@@ -300,17 +386,15 @@ public class GameTest extends ApprovalsBase {
         write("<tr>\n"+IntStream.range(0, columns)
                 .mapToObj(position -> String.format("<td class=\"%s\">%s</td>",
                         aGame.category(position).toLowerCase(),
-                        getListPlayerAt(aGame, position, playerHighLighted).stream().collect(Collectors.joining(""))))
+                        formatPlayers(getListPlayerAt(aGame, position, playerHighLighted), "")))
                 .collect(Collectors.joining("\n"))
                 + "\n</tr>\n"
         );
         write("</table>\n\n++++\n\n");
     }
     private void displayPlayers(Game aGame) throws IOException {
-        write("Players : \n\n");
-
-        write(aGame.players.stream()
-                .collect(Collectors.joining(", "))
+        write("Players : ");
+        write(formatPlayers(aGame.players, ", ")
                 + "\n\n"
         );
     }
