38b778faf44d926242649a227427b7927ef5ff53
sfauvel <sfauvel@gmail.com>
2020-12-04 17:46:28 +0100

    Move program to display unicode

--- documentationtesting/src/main/java/org/sfvl/docformatter/ViewUnicode.java
+++ documentationtesting/src/main/java/org/sfvl/docformatter/ViewUnicode.java
@@ -1,5 +1,9 @@
-package org.sfvl.doctesting;
+package org.sfvl.docformatter;
 
+/**
+ * A program to display Unicode in the output.
+ * You can copy output in a `.adoc` file to see how it is displayed.
+ */
 public class ViewUnicode {
 
     public static void show() {
