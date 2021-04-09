78dd877fc9697fc157fd076a2e8ea116847e8781
sfauvel <sfauvel@gmail.com>
2021-04-09 08:36:47 +0200

    Migrate InstallingLibrary as a DocumentProducer

--- documentationtesting/src/test/java/org/sfvl/howto/InstallingLibrary.java
+++ documentationtesting/src/test/java/org/sfvl/howto/InstallingLibrary.java
@@ -1,26 +1,36 @@
 package org.sfvl.howto;
 
+import org.sfvl.docformatter.AsciidocFormatter;
+import org.sfvl.docformatter.Formatter;
 import org.sfvl.doctesting.writer.Document;
+import org.sfvl.doctesting.writer.DocumentProducer;
 import org.sfvl.doctesting.writer.DocumentationBuilder;
+import org.sfvl.doctesting.writer.Options;
 
 import java.io.IOException;
 
-public class InstallingLibrary extends DocumentationBuilder {
+public class InstallingLibrary implements DocumentProducer {
 
-    public InstallingLibrary() {
-        super("Installing Documentation testing");
+    Formatter formatter = new AsciidocFormatter();
 
-        withStructureBuilder(InstallingLibrary.class,
-                b -> b.getDocumentOptions(),
-                b -> String.format("= %s\n", b.getDocumentTitle()),
-                b -> b.getContent());
+    public String build() {
+        return formatter.paragraphSuite(
+                new Options(formatter).withCode(),
+                formatter.title(1, "Installing Documentation testing"),
+                getContent()
+        );
     }
 
     protected String getContent() {
         return "To be written";
     }
 
+    @Override
+    public void produce() throws IOException {
+        new Document(build()).saveAs(this.getClass());
+    }
+
     public static void main(String... args) throws IOException {
-        Document.produce(new InstallingLibrary());
+        new InstallingLibrary().produce();
     }
 }
