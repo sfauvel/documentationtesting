665f010647bbb8d4aebd468366c46fea601c9acf
sfauvel <sfauvel@gmail.com>
2021-04-24 00:46:19 +0200

    Add some information to install library

--- documentationtesting/src/test/java/org/sfvl/howto/InstallingLibrary.java
+++ documentationtesting/src/test/java/org/sfvl/howto/InstallingLibrary.java
@@ -21,7 +21,35 @@ public class InstallingLibrary implements DocumentProducer {
     }
 
     protected String getContent() {
-        return "To be written";
+        final String dependency = "        <dependency>\n" +
+                "            <groupId>org.sfvl</groupId>\n" +
+                "            <artifactId>documentationtesting</artifactId>\n" +
+                "            <version>${documentationtesting.version}</version>\n" +
+                "            <scope>test</scope>\n" +
+                "        </dependency>";
+
+        return "We use the library link:https://github.com/sfauvel/documentationtesting[documentationtesting]\n" +
+                "which provides what we need to get started.\n" +
+                "For a complete description of how to use it go to the link:https://sfauvel.github.io/documentationtesting/documentationtesting[documentation site]\n" +
+                "\n" +
+                "The library is not yet on a public repository.\n" +
+                "To use it, you need to download and install it.\n" +
+                "\n" +
+                "You have to download `.jar` and `.pom` from\n" +
+                "link:https://github.com/sfauvel/documentationtesting/packages/538792[last Documentation Testing release]\n" +
+                "\n" +
+                "Run this maven command to install it:\n" +
+                "----\n" +
+                "mvn install:install-file -Dfile=<path-to-file>.jar -DpomFile=<path-to-pomfile>.pom\n" +
+                "----\n" +
+                "\n" +
+                "\n" +
+                "Then, you can add the dependency to your `pom.xml` to use it in your project" +
+                "\n" +
+                formatter.sourceCodeBuilder("xml")
+                        .indent(0)
+                        .source(dependency)
+                        .build();
     }
 
     @Override
