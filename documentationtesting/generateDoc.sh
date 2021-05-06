ddd757dbd5ebdebd634542e4e8fd7ed10682876a
sfauvel <sfauvel@gmail.com>
2021-05-06 13:38:25 +0200

    Generate for all .adoc files except .approved.adoc ones

--- documentationtesting/generateDoc.sh
+++ documentationtesting/generateDoc.sh
@@ -8,12 +8,16 @@ DESTINATION_PATH=../docs/${PROJECT_NAME}
 
 mvn clean install package -q
 
-for DOC_FILE in $(find ${DOCS_PATH} -name "*[^\.approved].adoc")
+for DOC_FILE in $(find ${DOCS_PATH} -type f -name "*.adoc")
 do
-  ASCIIDOC_NAME=${DOC_FILE##*/}
-  HTMLDOC_NAME=${ASCIIDOC_NAME/.adoc/.html}
-  ASCIIDOC_PATH=${DESTINATION_PATH}$(dirname ${DOC_FILE})
-  ASCIIDOC_PATH=${ASCIIDOC_PATH/$DOCS_PATH/}
-  ROOT_PATH=$(dirname ${DOC_FILE})
-  ${SCRIPTS_PATH}/convertAdocToHtml.sh  ${ROOT_PATH} ${ASCIIDOC_NAME} ${ASCIIDOC_PATH} ${HTMLDOC_NAME}
+  # Not include approved files
+  if [[ ! "$DOC_FILE" == *.approved.adoc ]]
+  then
+    ASCIIDOC_NAME=${DOC_FILE##*/}
+    HTMLDOC_NAME=${ASCIIDOC_NAME/.adoc/.html}
+    ASCIIDOC_PATH=${DESTINATION_PATH}$(dirname ${DOC_FILE})
+    ASCIIDOC_PATH=${ASCIIDOC_PATH/$DOCS_PATH/}
+    ROOT_PATH=$(dirname ${DOC_FILE})
+    ${SCRIPTS_PATH}/convertAdocToHtml.sh  ${ROOT_PATH} ${ASCIIDOC_NAME} ${ASCIIDOC_PATH} ${HTMLDOC_NAME}
+  fi
 done
