26a4fc1140aedcfa5c06005e2d3db0a65be2c31e
sfauvel <sfauvel@gmail.com>
2021-07-06 13:22:54 +0200

    Add a standard Approvals test on GildedRose

--- samples/demo_gildedrose/src/main/java/fr/xebia/katas/gildedrose/Inn.java
+++ samples/demo_gildedrose/src/main/java/fr/xebia/katas/gildedrose/Inn.java
@@ -71,7 +71,7 @@ public class Inn {
          }
       }
 
-}
+   }
 
    public static void main(String[] args) {
       System.out.println("OMGHAI!");
---
+++ samples/demo_gildedrose/src/test/java/fr/xebia/katas/gildedrose/TestUpdate.java
@@ -0,0 +1,24 @@
+package fr.xebia.katas.gildedrose;
+
+import org.approvaltests.Approvals;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+
+public class TestUpdate {
+
+    @Test
+    public void test_update_item() {
+        Inn inn = new Inn();
+        StringBuffer output = new StringBuffer();
+        for (int i = 1; i < 20; i++) {
+            for (Item item : inn.getItems()) {
+                output.append(String.format("%s,%d,%d\n", item.getName(), item.getSellIn(), item.getQuality()));
+            }
+            inn.updateQuality();
+        }
+
+        Approvals.verify(output);
+    }
+
+}
