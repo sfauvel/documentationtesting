97523931140ce1e33a376ed7da4ac181d583045b
sfauvel <sfauvel@gmail.com>
2021-08-04 18:49:39 +0200

    Do not display temporary folder value but a place holder to have the same result on different OS

--- documentationtesting/src/test/java/org/sfvl/doctesting/junitextension/FailureReporterTest.java
+++ documentationtesting/src/test/java/org/sfvl/doctesting/junitextension/FailureReporterTest.java
@@ -138,7 +138,7 @@ public class FailureReporterTest {
 
         doc.write(formatter.blockBuilder(Formatter.Block.LITERAL)
                 .title("Report")
-                .content(report.replaceAll("/tmp/.*/files/", "/tmp/files/").trim())
+                .content(report.replaceAll(tempDir + "/files/", "[TEMPORARY FOLDER]/files/").trim())
                 .build());
     }
 }
