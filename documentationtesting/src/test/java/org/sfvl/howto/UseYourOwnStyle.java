ba4019958827ba5d15d252e7b346c543d72d7bce
sfauvel <sfauvel@gmail.com>
2021-08-12 00:29:12 +0200

    Improve documentation to customize style

--- documentationtesting/src/test/java/org/sfvl/howto/UseYourOwnStyle.java
+++ documentationtesting/src/test/java/org/sfvl/howto/UseYourOwnStyle.java
@@ -3,9 +3,8 @@ package org.sfvl.howto;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.sfvl.docformatter.AsciidocFormatter;
-import org.sfvl.doctesting.junitextension.SimpleApprovalsExtension;
-import org.sfvl.doctesting.utils.DocWriter;
 import org.sfvl.doctesting.junitextension.ApprovalsExtension;
+import org.sfvl.doctesting.junitextension.SimpleApprovalsExtension;
 
 /**
  * To have specific style in some chapter, you can use HTML style directly in your Asciidoc files.
@@ -19,6 +18,11 @@ public class UseYourOwnStyle {
 
     private final AsciidocFormatter formatter = new AsciidocFormatter();
 
+    /**
+     * You can customize style adding a css style directly in your file.
+     * You need to put an html style tag (`<style></style>`) enclosed in `\++++++`
+     * to add it without processed content and send it directly to the output.
+     */
     @Test
     public void define_a_custom_style() {
         final String style = String.join("\n",
@@ -29,27 +33,51 @@ public class UseYourOwnStyle {
                 "}",
                 "</style>",
                 "++++");
+        {
+            final String text = String.join("\n",
+                    "This text should be black.\n",
+                    "[.withColor]#This text shoud be blue.#\n",
+                    "This text is not anymore blue."
+            );
 
-        final String text = String.join("\n\n",
-                "This text should be black.",
-                "[withColor]#This text shoud be blue.#",
-                "This text is not anymore blue."
-        );
+            final String content = String.format("%s\n%s", style, text);
 
-        final String content = String.format("%s\n\n%s", style, text);
+            doc.write(formatter.sourceCodeBuilder("html")
+                    .title("Asciidoc content")
+                    .source(content)
+                    .build(),
+                    "", "");
 
-        doc.write(formatter.sourceCodeBuilder("html")
-                .title("Asciidoc content")
-                .source(content)
-                .build());
+            doc.write(formatter.blockBuilder("====")
+                    .title("Content rendering")
+                    .content(content)
+                    .build(),
+                    "", "");
+        }
+        {
+            final String text = String.join("\n",
+                    "This text should be black.",
+                    "[.withColor]",
+                    "--",
+                    "This section formed by several lines\n",
+                    "should be blue.",
+                    "--",
+                    "This text is not anymore blue."
+            );
 
-        doc.write("\n\n");
+            final String content = String.format("%s\n%s", style, text);
 
-        doc.write(formatter.blockBuilder("====")
-                .title("Content rendering")
-                .content(content)
-                .build());
+            doc.write(formatter.sourceCodeBuilder("html")
+                    .title("Asciidoc content")
+                    .source(content)
+                    .build(),
+                    "", "");
 
-
+            doc.write(formatter.blockBuilder("====")
+                    .title("Content rendering")
+                    .content(content)
+                    .build(),
+                    "", "");
+        }
     }
 }
