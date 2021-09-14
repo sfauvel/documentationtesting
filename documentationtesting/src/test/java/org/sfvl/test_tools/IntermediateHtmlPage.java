ddd74439fbbe5f9cde9c706cac35f1277d7e0e4b
Your Name <sfauvel@gmail.com>
2021-09-14 12:12:18 +0200

    Uniformize theme on documentationTesting project

--- documentationtesting/src/test/java/org/sfvl/test_tools/IntermediateHtmlPage.java
+++ documentationtesting/src/test/java/org/sfvl/test_tools/IntermediateHtmlPage.java
@@ -15,6 +15,7 @@ public class IntermediateHtmlPage extends HtmlPageExtension {
                 ":nofooter:",
                 ":stem:",
                 ":source-highlighter: rouge",
+                ":rouge-style: thankful_eyes",
                 ":toclevels: 4",
                 "",
                 String.format("include::%s[]", new DocPath(clazz).approved().fullname()),
