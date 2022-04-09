a5708763e9c4aece9749480a7d79488e9d376886
Your Name <sfauvel@gmail.com>
2022-04-09 22:19:03 +0200

    Improve FizzBuzz documentation

--- samples/demo_fizzbuzz/src/test/java/org/sfvl/application/fizzbuzz/FizzBuzzTest.java
+++ samples/demo_fizzbuzz/src/test/java/org/sfvl/application/fizzbuzz/FizzBuzzTest.java
@@ -1,12 +1,15 @@
 package org.sfvl.application.fizzbuzz;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.sfvl.application.FizzBuzz;
 import org.sfvl.doctesting.junitinheritance.ApprovalsBase;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.function.IntFunction;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -28,42 +31,61 @@ public class FizzBuzzTest extends ApprovalsBase {
         write("... etc up to 100");
     }
 
-    /**
-     * When number is not divisible by five or three, returns the number.
-     */
     @Test
-    public void return_given_number() {
-        callFizzBuzzWith(1);
-        callFizzBuzzWith(2);
-        callFizzBuzzWith(4);
-        callFizzBuzzWith(22);
+    public void output_in_a_table() {
+
+        final int lastValue = 20;
+        Function<IntFunction<String>, String> toLine = f -> IntStream.rangeClosed(1, lastValue)
+                .mapToObj(f)
+                .collect(Collectors.joining(" | ", "| ", ""));
+
+        write("[%autowidth,cols=" + lastValue + "*^]",
+                "|====",
+                toLine.apply(Integer::toString),
+                toLine.apply(FizzBuzz::get),
+                "|====");
+
     }
 
-    /**
-     * When number is divisible by three, returns Fizz.
-     */
-    @Test
-    public void return_fizz_when_divisible_by_three() {
-        callFizzBuzzWith(3);
-        callFizzBuzzWith(9);
-    }
+    @Nested
+    class Rules {
+        /**
+         * When number is not divisible by five or three, returns the number.
+         */
+        @Test
+        public void return_given_number() {
+            callFizzBuzzWith(1);
+            callFizzBuzzWith(2);
+            callFizzBuzzWith(4);
+            callFizzBuzzWith(22);
+        }
 
-    /**
-     * When number is divisible by five, returns Buzz.
-     */
-    @Test
-    public void return_buzz_when_divisible_by_five() {
-        callFizzBuzzWith(5);
-        callFizzBuzzWith(25);
-    }
+        /**
+         * When number is divisible by three, returns Fizz.
+         */
+        @Test
+        public void return_fizz_when_divisible_by_three() {
+            callFizzBuzzWith(3);
+            callFizzBuzzWith(9);
+        }
 
-    /**
-     * When number is divisible by three and five, returns FizzBuzz.
-     */
-    @Test
-    public void return_fizzbuzz_when_divisible_by_three_and_five() {
-        callFizzBuzzWith(15);
-        callFizzBuzzWith(60);
+        /**
+         * When number is divisible by five, returns Buzz.
+         */
+        @Test
+        public void return_buzz_when_divisible_by_five() {
+            callFizzBuzzWith(5);
+            callFizzBuzzWith(25);
+        }
+
+        /**
+         * When number is divisible by three and five, returns FizzBuzz.
+         */
+        @Test
+        public void return_fizzbuzz_when_divisible_by_three_and_five() {
+            callFizzBuzzWith(15);
+            callFizzBuzzWith(60);
+        }
     }
 
     @Test
@@ -81,8 +103,8 @@ public class FizzBuzzTest extends ApprovalsBase {
         write(String.format("List of values (between & to %s) that give a specific result", maxValue),
                 "",
                 groupedValues.entrySet().stream()
-                .map(e -> String.format("* *%s*: %s", e.getKey(), e.getValue()))
-                .collect(Collectors.joining("\n")));
+                        .map(e -> String.format("* *%s*: %s", e.getKey(), e.getValue()))
+                        .collect(Collectors.joining("\n")));
     }
 
     public static boolean isNumeric(String strNum) {
