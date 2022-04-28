f0fcce1675b49fdf34fcb7d72f4c229069b817dc
Your Name <sfauvel@gmail.com>
2022-04-28 23:41:43 +0200

    Add general approch used to develop this tool

--- documentationtesting/src/test/java/org/sfvl/development/Development.java
+++ documentationtesting/src/test/java/org/sfvl/development/Development.java
@@ -8,7 +8,12 @@ public class Development extends PackagePage {
     @Test
     public void introduction() {
         doc.write("This section gives some information for developers who work on this library.",
-                "We describe architecture, development tools, how we organize tests and documentation, ...");
+                "We describe architecture, development tools, how we organize tests and documentation, ...",
+                "",
+                "We follow some general principals to develop this tool:",
+                "",
+                "* Everything is test using the `doc as test` approach.",
+                "* We avoid to write a value as String when we can extract it.");
     }
 
     @Test
