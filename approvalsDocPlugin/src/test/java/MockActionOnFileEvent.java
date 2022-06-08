af0574fb603f2d4c10f3d8418a734d727de5f4b9
Your Name <sfauvel@gmail.com>
2022-06-08 09:13:48 +0200

    Refactor mock to be able to select multiple file

--- approvalsDocPlugin/src/test/java/MockActionOnFileEvent.java
+++ approvalsDocPlugin/src/test/java/MockActionOnFileEvent.java
@@ -1,28 +1,29 @@
-import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.openapi.actionSystem.DataKey;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiFile;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.mockito.Mockito;
+
+import java.util.List;
 
 class MockActionOnFileEvent extends MockActionEvent {
 
-    public MockActionOnFileEvent(Project project, VirtualFile virtualFile, PsiFile psiFile) {
+    public MockActionOnFileEvent(Project project) {
         super(project);
-        Mockito.when(getDataContext().getData(PlatformDataKeys.PSI_FILE)).thenReturn(psiFile);
-        Mockito.when(getDataContext().getData(PlatformDataKeys.VIRTUAL_FILE)).thenReturn(virtualFile);
-    }
-
-    public MockActionOnFileEvent(PsiFile psiFile) {
-        this(psiFile.getProject(), psiFile.getVirtualFile(), psiFile);
     }
 
     public MockActionOnFileEvent(Project project, VirtualFile virtualFile) {
-        this(project, virtualFile, null);
+        this(project);
+        Mockito.when(getDataContext().getData(PlatformDataKeys.VIRTUAL_FILE)).thenReturn(virtualFile);
     }
 
+    public MockActionOnFileEvent(Project project, List<VirtualFile> virtualFiles) {
+        this(project);
+        Mockito.when(getDataContext().getData(PlatformDataKeys.VIRTUAL_FILE_ARRAY)).thenReturn(virtualFiles.toArray(new VirtualFile[0]));
+    }
+
+    public MockActionOnFileEvent(PsiFile psiFile) {
+        this(psiFile.getProject(), psiFile.getVirtualFile());
+        Mockito.when(getDataContext().getData(PlatformDataKeys.PSI_FILE)).thenReturn(psiFile);
+    }
 }
