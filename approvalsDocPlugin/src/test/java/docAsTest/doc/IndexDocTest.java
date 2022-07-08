9ff86d76a6bd47c2a487c6442d7f09789ed2677d
Your Name <sfauvel@gmail.com>
2022-07-08 22:28:46 +0200

    Add a documentation index file for the intelij plugin

---
+++ approvalsDocPlugin/src/test/java/docAsTest/doc/IndexDocTest.java
@@ -0,0 +1,54 @@
+package docAsTest.doc;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.sfvl.docformatter.asciidoc.AsciidocFormatter;
+import org.sfvl.doctesting.junitextension.HtmlPageExtension;
+import org.sfvl.doctesting.utils.Config;
+import org.sfvl.doctesting.utils.DocPath;
+import org.sfvl.doctesting.utils.NoTitle;
+import org.sfvl.doctesting.writer.DocWriter;
+import tools.ApprovalsJUnit4;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RunWith(JUnit4.class)
+public class IndexDocTest extends ApprovalsJUnit4 {
+
+    AsciidocFormatter formatter = new AsciidocFormatter();
+
+    @AfterClass
+    public static void tearDown() throws IOException {
+        new HtmlPage().generate(IndexDocTest.class);
+    }
+
+    @Test
+    @NoTitle
+    public void test_content() {
+        write(formatter.include(approvedDocPathStringFrom(ShortCutTest.class, this.getClass())));
+    }
+
+    private String approvedDocPathStringFrom(Class<?> targetClass, Class<?> fromClass) {
+        return DocPath.toAsciiDoc(new DocPath(targetClass).approved().from(fromClass));
+    }
+
+}
+
+class HtmlPage extends HtmlPageExtension {
+    @Override
+    public String content(Class<?> clazz) {
+        return String.join("\n",
+                new DocWriter<AsciidocFormatter>(new AsciidocFormatter()).defineDocPath(Paths.get(".")),
+                ":nofooter:",
+                super.content(clazz));
+    }
+
+    @Override
+    public Path getFilePath(Class<?> clazz) {
+        return Config.DOC_PATH.resolve("index.adoc");
+    }
+}
