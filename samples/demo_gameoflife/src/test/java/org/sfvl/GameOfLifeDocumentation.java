6982736927fbbcd49d964bf7af5704a1608132c7
Your Name <sfauvel@gmail.com>
2022-09-23 09:25:24 +0200

    Move DemoDocumentation from the main project to a project used only by demo samples

--- samples/demo_gameoflife/src/test/java/org/sfvl/GameOfLifeDocumentation.java
+++ samples/demo_gameoflife/src/test/java/org/sfvl/GameOfLifeDocumentation.java
@@ -5,9 +5,6 @@ import org.sfvl.doctesting.utils.Config;
 import org.sfvl.doctesting.writer.Document;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Collectors;
 
 public class GameOfLifeDocumentation extends DemoDocumentation {
 
@@ -25,13 +22,6 @@ public class GameOfLifeDocumentation extends DemoDocumentation {
 
     @Override
     public String build() {
-//        String style = "";
-//        try {
-//            style = Files.lines(Paths.get("src", "test", "resources", "style.css"))
-//                    .collect(Collectors.joining("\n"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
         return super.build() + "\n" + include_style();
     }
 
